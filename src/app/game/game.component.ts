fe743c58e0655d9adde261998850826e5c0d7bcb
Patrick Rush <mail@patrickrush.tech>
2024-03-31 18:15:42 -0400

    Add color vs b&w option

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,46 +1,40 @@
-import {
-  CommonModule,
-  NgFor,
-  NgIf,
-  NgStyle,
-  NgTemplateOutlet,
-} from '@angular/common';
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { CommonModule, NgFor, NgIf, NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
 import { ColorCellPipe } from '../color-cell.pipe';
 
+type BoardMap = Map<number, Map<number, [boolean]>>;
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [
-    ColorCellPipe,
-    CommonModule,
-    NgFor,
-    NgIf,
-    NgStyle,
-    NgTemplateOutlet,
-  ],
+  imports: [ColorCellPipe, CommonModule, NgFor, NgIf, NgStyle],
+  providers: [ColorCellPipe],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css',
 })
 export class GameComponent {
-  intervalId: number | null = null;
   private readonly interval: number = 100;
-  private readonly boardSize: number = 55;
+  private readonly boardSize: number = 60; // Board must be a positive, even number
+  private intervalId: number | null = null;
   running: boolean = false;
   iteration: number = 0;
   livingCells: number = 0;
   board: [boolean][][];
-  boardMap: Map<number, Map<number, [boolean]>>;
+  boardMap: BoardMap;
+  colorMode: boolean = false;
+  colorButtonHovered: boolean = false;
+  currentColor: string;
 
-  constructor(private ref: ChangeDetectorRef) {
+  constructor(private colorCell: ColorCellPipe) {
     const [newBoard, newBoardMap] = this.generateBoard();
     this.board = newBoard;
     this.boardMap = newBoardMap;
+    this.writeName();
+    this.currentColor = this.colorCell.transform(this.livingCells);
   }
 
-  generateBoard(): [[boolean][][], Map<number, Map<number, [boolean]>>] {
+  generateBoard(): [[boolean][][], BoardMap] {
     const newBoard: [boolean][][] = [];
-    const newBoardMap: Map<number, Map<number, [boolean]>> = new Map();
+    const newBoardMap: BoardMap = new Map();
     for (let i = 0; i < this.boardSize; i++) {
       const row: [boolean][] = [];
       const rowMap = new Map<number, [boolean]>();
@@ -72,6 +66,9 @@ export class GameComponent {
     this.intervalId = window.setInterval(this.runGame, this.interval);
     this.running = true;
     console.log('game started');
+
+    // testing
+    console.time('game');
   }
 
   stopGame() {
@@ -79,14 +76,16 @@ export class GameComponent {
     window.clearInterval(this.intervalId);
     this.running = false;
     console.log('game stopped');
+
+    //testing
+    console.timeEnd('game');
   }
 
   resetGame() {
     this.stopGame();
+    const [newBoard, newBoardMap] = this.generateBoard();
     this.iteration = 0;
     this.livingCells = 0;
-    this.board = [];
-    const [newBoard, newBoardMap] = this.generateBoard();
     this.board = newBoard;
     this.boardMap = newBoardMap;
     console.log('game reset');
@@ -94,13 +93,15 @@ export class GameComponent {
 
   runGame = () => {
     console.log('game running');
+
     this.iteration++;
     let newCellCount = 0;
-    const [newBoard, newBoardMap] = this.generateBoard();
     let gameOver = true;
+    const [newBoard, newBoardMap] = this.generateBoard();
+
     this.board.forEach((row, i) => {
       row.forEach((cell, j) => {
-        const newCell = newBoardMap.get(i)?.get(j)!;
+        const newCell = this.getCell(newBoardMap, i, j);
         const neighbors = this.getLiveNeighborCount(i, j);
         if (cell[0]) {
           gameOver = false;
@@ -117,31 +118,88 @@ export class GameComponent {
         if (newCell[0]) newCellCount++;
       });
     });
-    if (gameOver || this.iteration === 999999) this.stopGame();
+
+    // if (gameOver || this.iteration === 999999) this.stopGame();
+
+    //testing
+    if (gameOver || this.iteration === 100) this.stopGame();
+
     this.livingCells = newCellCount;
+    this.currentColor = this.colorCell.transform(this.livingCells);
     this.board = newBoard;
     this.boardMap = newBoardMap;
   };
 
   flipCell(row: number, col: number) {
-    const cell = this.boardMap.get(row)?.get(col);
-    if (cell === undefined) return;
-    cell[0] = !cell[0];
-    if (cell[0]) this.livingCells++;
+    const cell = this.getCell(this.boardMap, row, col);
+
+    if (!cell) return;
+
+    const newValue = !cell[0];
+    cell[0] = newValue;
+
+    if (newValue) this.livingCells++;
     else this.livingCells--;
-    this.ref.detectChanges();
   }
 
   getLiveNeighborCount(row: number, col: number): number {
     let liveNeighbors: number = 0;
-    for (let r = row - 1; r <= row + 1; r++) {
-      for (let c = col - 1; c <= col + 1; c++) {
+
+    for (let r = row - 1, rr = row + 1; r <= rr; r++) {
+      for (let c = col - 1, cc = col + 1; c <= cc; c++) {
         if (r === row && c === col) continue;
         if (r >= 0 && r < this.boardSize && c >= 0 && c < this.boardSize) {
-          if (this.boardMap.get(r)?.get(c)?.[0]) liveNeighbors++;
+          if (this.getCellValue(this.boardMap, r, c)) liveNeighbors++;
         }
       }
     }
+
     return liveNeighbors;
+  }
+
+  getCell(board: BoardMap, row: number, col: number): [boolean] {
+    return board.get(row)?.get(col)!;
+  }
+
+  getCellValue(board: BoardMap, row: number, col: number): boolean {
+    return this.getCell(board, row, col)[0]!;
+  }
+
+  flipColorMode() {
+    this.colorMode = !this.colorMode;
+  }
+
+  LIFE = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  writeName() {
+    const boardSizeReference = this.boardSize / 2;
+    const negativeSpace = this.LIFE.length;
+    const min = boardSizeReference - negativeSpace / 2;
+    for (let row = 0; row < negativeSpace; row++) {
+      for (let col = 0; col < negativeSpace; col++) {
+        if (this.LIFE[col][row]) this.flipCell(row + min, col + min);
+      }
+    }
   }
 }
